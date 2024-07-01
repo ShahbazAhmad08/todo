9da3a8cbe53adfeae674931a97cd6c5309316570
Shahbaz Ahmad <Sa798027@gmail.com>
2024-07-01 13:52:31 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import TaskTracker from './components/TaskTracker';
+
+function App() {
+  return (
+    <div className="App">
+     <TaskTracker/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TaskTracker.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import './TaskTracker.css'
+const TaskTracker = () => {
+    const [users,setusers]=useState([]);
+    const[name, setName]=useState("");
+    const[editIndex,setEditIndex]=useState(-1);
+    const addUser=()=>{
+                 if(editIndex!==-1){
+                    const userList=[...users];
+                    userList[editIndex]=name;
+                    if(name!==""){
+                    setusers(userList);
+                    setEditIndex(-1);
+                    }
+                 }
+                 else if(name!==""){
+                    const newUser=[...users,name];
+                    setusers(newUser)
+                 }
+        setName("");
+    }
+    const editUser=(index)=>{
+     setEditIndex(index);
+     setName(users[index]);
+    }
+    const deleteUser=(userIndex)=>{
+        const updateUser=users.filter((user,index)=>
+        index!==userIndex);
+        setusers(updateUser);
+    }
+  return (
+    <div className='TaskTracker'>
+        <h2 className='heading'>TaskTracker App</h2>
+        <div className='add-user'> 
+         <input type='text'
+                value={name}
+                onChange={(e)=>setName(e.target.value)} className='input-user' placeholder='write Your Task ✍️'/>
+                <i className="fa-solid fa-plus fa-xl add" onClick={addUser} ></i>
+        </div>
+
+        <div className='user-list'>
+            {users.map((user,index)=>(
+                <div key={index} className='task'>
+              <input type='checkbox' className='checkbox'/>
+              <p>{user}</p>
+              <i className="fa-solid fa-pen-to-square fa-xl edit" onClick={()=>editUser(index)}> </i>
+              <i className="fa-solid fa-trash fa-lg delete"onClick={()=>deleteUser(index)}></i>
+                </div>
+            ))}
+        </div>
+   
+        </div>
+
+
+  
+  )
+}
+
+export default TaskTracker
