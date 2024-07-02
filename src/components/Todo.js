9ffa5aa60441c6b8628cb76db0af9bbb173fb749
Shahbaz Ahmad <Sa798027@gmail.com>
2024-07-02 11:43:41 +0530

    change in files

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import './Todo.css'
+import './components/Todo.css'
 const Todo = () => {
     const [users,setusers]=useState([]);
     const[name, setName]=useState("");
